70c4c499c4ccbba0f57dfbedd0fd4a2ab1b3c735
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:53:33 -0300

    Add files via upload

---
+++ 86-MultiplicarValoresArrayYMostrar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+//Pedir un numero por teclado y multiplicar todos los valores de un array y mostrar el array.
+
+namespace _86_MultiplicarValoresArrayYMostrar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero por teclado para mutiplicar los valores del array:");
+
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[3];
+
+            array[0] = 1;
+            array[1] = 3;
+            array[2] = 6;
+
+            Console.Clear();
+
+            for (int q = 0; q < array.Length; q++)
+            {
+                Console.WriteLine("Los valores del array son: " + array[q]);
+            }
+
+            for (int q = 0; q < array.Length; q++)
+            {
+                Console.WriteLine("El resultado de multipicar por " + i +" la cuenta es: " + array[q]*i);
+            }
+        }
+    }
+}
